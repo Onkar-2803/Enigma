115d0ea1e5e0976838d53a884bbc249f7f8ce88f
Onkar Rupesh Litake <60955023+Onkar-2803@users.noreply.github.com>
2020-10-23 17:29:19 +0530

    Added Questions Page

---
+++ enigma_questions.js
@@ -0,0 +1,19 @@
+const startingMinutes=10;
+let time = startingMinutes*60;
+
+const countdownEl = document.getElementById('countdown');
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown(){
+    const minutes = Math.floor(time/60);
+    let seconds = time % 60;
+    seconds = seconds <10 ? '0' + seconds:seconds;
+    countdownEl.innerHTML= `${minutes}: ${seconds}`;
+    if(time<1)
+     {
+          clearTimeout(countdownEl);
+          element.innerHTML='<h1>Time Up!</h1>';
+     }
+    time--;
+}
